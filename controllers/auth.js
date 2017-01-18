0225b49ac0c337e17bfca4e07306c3e2cbb3cd44
skylocke <mathematical.fiction@gmail.com>
2017-01-18 14:53:43 -0800

    slight changes to navbar

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,7 +35,7 @@ router.get("/login", function(req, res) {
 });
 
 router.post("/login", passport.authenticate("local", {
-  successRedirect: "/dashboard",
+  successRedirect: "/",
   failureRedirect: "/auth/login",
   successFlash: "Logged in.",
   failureFlash: "Invalid username and/or password."
